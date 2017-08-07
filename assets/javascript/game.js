8919cee0cf030e7d84f1d3b7748fec5e8d49716c
gcdwyer <gcdwyer@gmail.com>
2017-08-06 22:20:06 -0400

    added attack button and logic to fight

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,29 +9,28 @@ $(document).ready(function() {
 
   var allCharacters = [
       ron = {
-          name: "Ron Burgundy",
+          name: "Ron",
           hp: 110,
           attack: 12,
           cAttack: 8,
-          id: "#darthVader-d"
       }, 
 
       brian = {
-          name: "Brian Fantana",
+          name: "Brian",
           hp: 95,
           attack: 9,
           cAttack: 9,
       }, 
 
       champ = {
-          name: "Champ Kind",
+          name: "Champ",
           hp: 125,
           attack: 8,
           cAttack: 6,
       }, 
 
       brick = {
-          name: "Brick Tamland",
+          name: "Brick",
           hp: 115,
           attack: 15,
           cAttack: 12,
@@ -44,8 +43,12 @@ $(document).ready(function() {
     $("#brickHP").html("HP: " + brick.hp);
 
 
-    var hero;
+    var hero = {};
     var enemy;
+    var counter = 0;
+    var heroChosen = false;
+    var enemyChosen = false;
+    var defender = [];
 
 
 
@@ -53,59 +56,76 @@ $(document).ready(function() {
 	
 	function pickHero() {
 		$(".characters").on("click", function () {
-			var heroPicked = $(this).attr("id");
-			console.log("Hero Picked: " + heroPicked);
 
-			if (heroPicked === "ronB") {
-				console.log ("You picked:" + heroPicked);
+			if (heroChosen == false && counter === 0) {
 
-				hero = ron;
-				console.log("var hero: " + hero);
+				heroChosen = true;
 
-				// redraw hero to yourCharacter div
-				$("#ronB").appendTo("#yourCharacter").attr("class", "imgBodyGood");
-				// redraw enemies to enemyCharacter div
-				$("#brianF, #champK, #brickT").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
+				counter++;
 
-				console.log("time to pick a defender");
+				var heroPicked = $(this).attr("id");
 
-				pickEnemy();
+				if (heroPicked === "ronB") {
+					console.log ("You picked:" + heroPicked);
 
+					hero = ron;
+					// console.log("var hero: " + hero);
 
-			} else if (heroPicked === "brianF") {
-				console.log ("You picked:" + heroPicked);
+					// redraw hero to yourCharacter div
+					$("#ronB").appendTo("#yourCharacter").attr("class", "imgBodyGood");
+					// redraw enemies to enemyCharacter div
+					$("#brianF, #champK, #brickT").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
 
-				hero = brian;
-				console.log("var hero: " + hero);
-
-				// redraw hero to yourCharacter div
-				$("#brianF").appendTo("#yourCharacter").attr("class", "imgBodyGood");
-				// redraw enemies to enemyCharacter div
-				$("#ronB, #champK, #brickT").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
+					pickEnemy();
 
 
-			} else if (heroPicked === "champK") {
-				console.log ("You picked:" + heroPicked);
+				} else if (heroPicked === "brianF") {
+					console.log ("You picked:" + heroPicked);
 
-				hero = champ;
-				console.log("var hero: " + hero);
+					hero = brian;
+					// console.log("var hero: " + hero);
 
-				// redraw hero to yourCharacter div
-				$("#champK").appendTo("#yourCharacter").attr("class", "imgBodyGood");
-				// redraw enemies to enemyCharacter div
-				$("#ronB, #brianF, #brickT").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
+					// redraw hero to yourCharacter div
+					$("#brianF").appendTo("#yourCharacter").attr("class", "imgBodyGood");
+					// redraw enemies to enemyCharacter div
+					$("#ronB, #champK, #brickT").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
+
+					pickEnemy();
 
 
-			} else if (heroPicked === "brickT") {
-				console.log ("You picked:" + heroPicked);
+				} else if (heroPicked === "champK") {
+					console.log ("You picked:" + heroPicked);
 
-				hero = brick;
-				console.log("var hero: " + hero);
+					hero = champ;
+					// console.log("var hero: " + hero);
 
-				// redraw hero to yourCharacter div
-				$("#brickT").appendTo("#yourCharacter").attr("class", "imgBodyGood");
-				// redraw enemies to enemyCharacter div
-				$("#ronB, #brianF, #champK").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
+					// redraw hero to yourCharacter div
+					$("#champK").appendTo("#yourCharacter").attr("class", "imgBodyGood");
+					// redraw enemies to enemyCharacter div
+					$("#ronB, #brianF, #brickT").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
+
+					pickEnemy();
+
+
+				} else if (heroPicked === "brickT") {
+					console.log ("You picked:" + heroPicked);
+
+					hero = brick;
+					// console.log("var hero: " + hero);
+
+					// redraw hero to yourCharacter div
+					$("#brickT").appendTo("#yourCharacter").attr("class", "imgBodyGood");
+					// redraw enemies to enemyCharacter div
+					$("#ronB, #brianF, #champK").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
+
+					pickEnemy();
+				}
+
+					console.log("hero chosen? " + heroChosen);
+
+					console.log("counter: " + counter);
+
+					console.log("time to pick an enemy");
 			}
 		});
 	};
@@ -115,41 +135,85 @@ $(document).ready(function() {
 
 	function pickEnemy() {
 
-		console.log("time to pick an enemy");
+			if ($("#ronB").on("click", function () {
 
-		$("#enemyCharacter").on("click", function () {
+	            if (heroChosen == true && enemyChosen == false) {
+	            	console.log("You chose Ron");
+	                $("#ronB").appendTo("#defenderArea");
+	                defender = ron;
+	                console.log("defender: " + defender);
+	                enemyChosen = true;
+	                fight();
+	            }
 
-			var enemyPicked = $(this).attr("id");
-			
-			console.log("Enemy Picked: " + enemyPicked);
+	        }));
 
-			if (enemyPicked === "ronB") {
+			if ($("#brianF").on("click", function () {
 
-				console.log ("You picked:" + enemyPicked);
+	            if (heroChosen == true && enemyChosen == false) {
+	            	console.log("You chose Brian");
+	                $("#brianF").appendTo("#defenderArea");
+	                defender = brian;
+	                console.log("defender: " + defender);
+	                enemyChosen = true;
+	                fight();
+	            }
+	            
+	        }));	
 
-				// hero = ron;
-				// console.log("var hero: " + hero);
+			if ($("#champK").on("click", function () {
 
-				// redraw hero to yourCharacter div
-				// $("#ronB").appendTo("#yourCharacter").attr("class", "imgBodyGood");
-				// redraw enemies to enemyCharacter div
-				// $("#brianF, #champK, #brickT").appendTo("#enemyCharacter").attr("class", "imgBodyBad");
+	            if (heroChosen == true && enemyChosen == false) {
+	            	console.log("You chose Champ");
+	                $("#champK").appendTo("#defenderArea");
+	                defender = champ;
+	                console.log("defender: " + defender);
+	                enemyChosen = true;
+	                fight();
+	            }
+	            
+	        }));	
 
-			} 
+			if ($("#brickT").on("click", function () {
+
+	            if (heroChosen == true && enemyChosen == false) {
+	            	console.log("You chose Brick");
+	                $("#brickT").appendTo("#defenderArea");
+	                defender = brick;
+	                console.log("defender: " + defender);
+	                enemyChosen = true;
+	                fight();
+	            }
+	            
+	        }));	
+
+    };
+
+
+// FUNCTION FIGHT ==========================================================================
+
+	function fight() {
+
+		console.log("Start FIght");
+		console.log("Hero HP: " + hero.hp);
+		console.log("Defender HP: " + defender.hp);
+
+		$("#attackButton").on("click", function () {
+
+		$("#yourCharacter").append("<p>" + hero.name + " attacks " + defender.name + " for " + hero.attack + " damage, but receives " + defender.attack + " damage!</p>");
+
 		});
+
 	};
-
-
 
 
 
 // RUNS GAME =================================================================================
 
-pickHero();
+	pickHero();
+
 
 });
-
-
 
 
 
